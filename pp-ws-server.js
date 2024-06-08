cc84a66d82ad8b4ad01b1544f7da852a1ed93289
quadkopter <krishnakanth.katakota@gmail.com>
2024-06-08 03:06:03 -0400

    Created demo websocket server program

---
+++ pp-ws-server.js
@@ -0,0 +1,17 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+  });
+
+  ws.on("connection", function message(conn) {
+    console.log("received: %s", conn);
+  });
+
+  ws.send("something");
+});
