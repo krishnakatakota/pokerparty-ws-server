2dc0f5e1d233175521ed3167cc3b1db4affdb6c3
Krishna Katakota <krk131@pitt.edu>
2024-07-19 16:29:22 -0400

    updated to use certificates in EC2 instance

--- pp-ws-server.js
+++ pp-ws-server.js
@@ -1,38 +1,53 @@
-import { WebSocketServer } from "ws";
+// import { WebSocketServer } from "ws";
 
-// Change this to work off of a websocket URL in an environment file when deploying!
-const server = new WebSocketServer({ port: 8081 });
+// // Change this to work off of a websocket URL in an environment file when deploying!
+// const server = new WebSocketServer({ port: 8081 });
 
-server.on("connection", function connection(ws) {
-  ws.on("error", console.error);
+const fs = require('fs');
+const https = require('https');
+const WebSocket = require('ws');
 
-  ws.on("message", (message) => {
-    console.log("%s", message);
+const server = https.createServer({
+	cert: fs.readFileSync('/etc/letsencrypt/live/pokerparty.click/fullchain.pem'),
+	key: fs.readFileSync('/etc/letsencrypt/live/pokerparty.click/privkey.pem')
+});
 
-	var messageString = new String(message.toString());
+const wss = new WebSocket.Server({ server });
 
-	server.clients.forEach((client) => {
-		var containsPLStringResult = containsPLString(messageString);
+wss.on("connection", function connection(ws) {
+	ws.on("error", console.error);
 
-		console.log(containsPLStringResult);
-		if (containsPLStringResult) {
-			// 0 means game state update
-			if (client !== ws) {
-				client.send("0:" + message);
+	ws.on("message", (message) => {
+		console.log("%s", message);
+
+		var messageString = new String(message.toString());
+
+		wss.clients.forEach((client) => {
+			var containsPLStringResult = containsPLString(messageString);
+
+			console.log(containsPLStringResult);
+			if (containsPLStringResult) {
+				// 0 means game state update
+				if (client !== ws) {
+					client.send("0:" + message);
+				}
+			} else {
+				// 1 means player join request
+				if (client !== ws) {
+					client.send("1:" + message);
+				}
 			}
-		} else {
-			// 1 means player join request
-			if (client !== ws) {
-				client.send("1:" + message);
-			}
-		}
-	})
-  });
+		})
+	});
 
-  ws.on("connection", function message(conn) {
-    console.log("received: %s", conn);
-  });
+	ws.on("connection", function message(conn) {
+		console.log("received: %s", conn);
+	});
 
+});
+
+server.listen(8081, () => {
+	console.log('Server is listening on port 8081');
 });
 
 function containsPLString(str) {
